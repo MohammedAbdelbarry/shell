fe109bfabb5c1cbdf187137cf07817b1b48abc97
Mohammed Abdelbarry <m7md337@gmail.com>
2017-10-05 20:37:12 +0200

    Added wordexp

--- command_parser.c
+++ command_parser.c
@@ -4,6 +4,7 @@
 #include "variables.h"
 #include <stdio.h>
 #include <string.h>
+#include <wordexp.h>
 
 
 bool isAssignment(char* command) {
@@ -28,24 +29,40 @@ struct Command parse_command(char *command) {
             break;
         }
     }
+    parsedCommand.isBackground = false;
     if (commandLen == 0) {
         parsedCommand.type = COMMENT;
         return parsedCommand;
     }
-    char **argv = shellSplit(command);
+    wordexp_t expandedLine;
+    int ret = wordexp(command, &expandedLine, WRDE_SHOWERR);
+    char **argv = expandedLine.we_wordv;//shellSplit(command);
+    switch(ret) {
+        case WRDE_BADCHAR:
+            printf("%s: error, invalid characters", SHELL_NAME);
+            parsedCommand.type = COMMENT;
+            return parsedCommand;
+        case WRDE_SYNTAX:
+            printf("%s: error, invalid syntax", SHELL_NAME);
+            parsedCommand.type = COMMENT;
+            return parsedCommand;
+    }
     if (argv == NULL) {
         printf("%s: failed to allocate memory\n", SHELL_NAME);
+        parsedCommand.argv = NULL;
+        parsedCommand.argc = 0;
+        //wordfree(&expandedLine);
+        return parsedCommand;
     }
-    int counter = 0;
-    while (argv[counter] != NULL) {
-        if(argv[counter][0] == '$') {
-            argv[counter] = lookup_variable(argv[counter] + 1);
-            argv[counter] = argv[counter] == NULL ? "" : argv[counter];
-        }
-        counter++;
+
+    if (*argv == NULL || strlen(*argv) == 0) {
+        parsedCommand.type = COMMENT;
+        //wordfree(&expandedLine);
+        return parsedCommand;
     }
+
     parsedCommand.argv = argv;
-    parsedCommand.argc = counter;
+    parsedCommand.argc = expandedLine.we_wordc;
     if(strcmp(argv[0], "cd") == 0) {
         parsedCommand.type = CD;
     } else if (strcmp(argv[0], "echo") == 0) {
@@ -61,5 +78,6 @@ struct Command parse_command(char *command) {
     } else {
         parsedCommand.type = PROGRAM;
     }
+    //wordfree(&expandedLine);
     return parsedCommand;
 }
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "environment.h"
 #include "variables.h"
 #include "commands.h"
+#include "command.h"
+#include "command_parser.h"
+#include "constants.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include <unistd.h>
+#include <unistd.h>
+#include <sys/wait.h>
 //#include <stdlib.h>
 //#include "strutil.h"
 
@@ -28,14 +32,8 @@ int main(int argc, char *argv[])
             }
         }
         printf("\n");
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp(argv[0], argv);
-            printf("ERROR");
-            abort();
-    }*/
+        */
     setup_environment();
-    print_all_variables();
     // any other early configuration should be here
     if( argc > 1 ){
         start(true);
@@ -50,8 +48,11 @@ int main(int argc, char *argv[])
 void start(bool read_from_file)
 {
 	cd(""); // let shell starts from home
-
-	if(read_from_file){
+    /*print_all_variables();
+    char cwd[512];
+    char* str = getcwd(cwd, 512);
+    printf("CWD=%s\n", str);*/
+    if(read_from_file){
 		// file processing functions should be called from here
 
 		shell_loop(true);
@@ -61,24 +62,75 @@ void start(bool read_from_file)
 	}
 }
 
+void execute_program(struct Command command) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(command.argv[0], command.argv);
+        printf("ERROR");
+        abort();
+    } else if (pid > 0) {
+        if (!command.isBackground) {
+             wait(0);
+        }
+    }
+}
+
+void execute_cd(struct Command command) {
+    if (command.argc == 2) {
+        cd(command.argv[1]);
+        printf("%s\n", command.argv[1]);
+    } else if (command.argc == 1) {
+        cd("");
+    } else {
+        printf("cd: invalid number of arguments\n");
+    }
+}
+
+void execute_echo(struct Command command) {
+    for (int i = 1 ; i < command.argc ; i++) {
+        printf("%s ", command.argv[i]);
+    }
+    printf("\n");
+}
+
 void shell_loop(bool input_from_file)
 {
 	bool from_file = input_from_file;
-
-	while(true){
+    char* line = NULL;
+    size_t buf_size = 512;
+    while(true){
 		if(from_file){
 			//read next instruction from file
 
 			// if end of file {from_file = false; continue;}
 		}
 		else{
-			//read next instruction from console
-		}
+            char* pwd = lookup_variable("PWD");
+            printf("%s>%s> ", SHELL_NAME, pwd);
+            int retCode = getline(&line, &buf_size, stdin);
+            if (retCode < 1) {
+                continue;
+            }
+            line[retCode - 1] = '\0';
+        }
 
 		//parse your command here
-
+        struct Command parsedCommand = parse_command(line);
 		//execute your command here
-
+        switch(parsedCommand.type) {
+            case PROGRAM:
+                execute_program(parsedCommand);
+                break;
+            case CD:
+                execute_cd(parsedCommand);
+            case ECHO:
+                execute_echo(parsedCommand);
+            case COMMENT:
+            default:
+                break;
+        }
+        //free(line);
+        //free(parsedCommand.argv);
 		/*
 			you don't need to write all logic here, a better practice is to call functions,
 			each one contains a coherent set of logical instructions
